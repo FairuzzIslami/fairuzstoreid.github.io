7531582a812f774b4bb8de1f34ab49dbb00b06c0
Fairuz <firdauz2007123@gmail.com>
2024-05-02 14:37:46 +0700

    Change the JS code in the HTML to a JavaScript file

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('hello world')
+//bagian contact
+function kirimPesanKeWhatsapp(){
+    const linkToWhastapp = `https://wa.me/625799690454?text= hallo saya ${nama.value} dan pesan saya tentang toko anda, ${pesan.value}` 
+    window.open(linkToWhastapp,"_blank")
+    }
